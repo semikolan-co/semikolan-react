f9d3eef5acab76bc74d17800dc71514ad3f96fd7
ankurg132 <ankurg052@gmail.com>
2021-06-09 03:27:48 +0530

    minor changes

--- src/component/Map.js
+++ src/component/Map.js
@@ -6,8 +6,10 @@ function About(props) {
     <span><em>01.</em>&nbsp;Where you can Find us</span>
     <div className="line-horizontal" />
   </div>
-  <div className="aboutdiv">
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3666.5849582236356!2d77.4800777142544!3d23.22179151468185!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x397c419fc6811533%3A0x538a9d921f4071ae!2sHarsh%20Vishwakarma!5e0!3m2!1sen!2sin!4v1617978278256!5m2!1sen!2sin" style={{border: 0, width: '100%', aspectRatio: '4/3'}} allowFullScreen loading="lazy" />
+  <div className="aboutdiv"  data-aos="fade-right" data-aos-duration={500}>
+  <div>
+        <p style={{color: '#fff'}}>You'll see our address here soon. Till then, we're everywhere in the cloud :)</p>
+      </div>
   </div>
 </div>
 
