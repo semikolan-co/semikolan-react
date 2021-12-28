6a527f1357e7b87484bb030a6a39eda73a34e62a
Harsh Vishwakarma <64343449+meinhoonharsh@users.noreply.github.com>
2021-12-28 19:01:28 +0530

    Fixed Alt Text Warning

--- src/component/Header.js
+++ src/component/Header.js
@@ -35,7 +35,7 @@ class Header extends React.Component {
           onclick="location.replace('/')"
         >
           <Link to="/">
-            <img src={logo}  style={{ height: "40px" }} />
+            <img src={logo} alt="SemiKolan Logo" style={{ height: "40px" }} />
             <span style={{ position: "relative", left: 5, top: 3 }}>
               &nbsp;SemiKolan
             </span>
