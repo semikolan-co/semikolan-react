a9d5b499bfb544a90e11db4eb0e0bd2bf3949b63
Harsh Vishwakarma <64343449+meinhoonharsh@users.noreply.github.com>
2022-01-13 03:00:23 +0530

    Implemented MainLayout on Links Page

--- src/pages/LinksPage.js
+++ src/pages/LinksPage.js
@@ -1,28 +1,21 @@
-import React from 'react'
-import Header from '../component/Header'
-import Landing from '../component/Landing'
-import Footer from '../component/Footer'
-import { Helmet } from "react-helmet"
-import semikolan from '../json/semikolan.json'
-import Links from '../component/Links'
+import Landing from "../component/Landing";
+import Links from "../component/Links";
+import MainLayout from "../layouts/MainLayout";
 
 export default function LinksPage() {
-    return (
-        <>
-            <Header />
-            <Landing
-              lottiename="links"
-              redirect="#links"
-              p1=""
-              p2="Links"
-              p3="All in one Place"
-              buttontext=""
-            />
-            <Links />
-            <Footer social={semikolan.social} />
-            <Helmet>
-              <title>Links | SemiKolan | Never Stop Building</title>
-            </Helmet>
-        </>
-    )
+  return (
+    <>
+      <MainLayout title="Links | Semikolan">
+        <Landing
+          lottiename="links"
+          redirect="#links"
+          p1=""
+          p2="Links"
+          p3="All in one Place"
+          buttontext=""
+        />
+        <Links />
+      </MainLayout>
+    </>
+  );
 }
