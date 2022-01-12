7a53e3a3f10eaba03f1afd8e9d7a3d0634264714
Harsh Vishwakarma <64343449+meinhoonharsh@users.noreply.github.com>
2022-01-13 03:28:47 +0530

    Implemented Layout on Join Page

--- src/pages/JoinPage.js
+++ src/pages/JoinPage.js
@@ -1,28 +1,21 @@
-import React from 'react'
-import Header from '../component/Header'
-import Landing from '../component/Landing'
-import Footer from '../component/Footer'
-import Join from '../component/Join'
-import { Helmet } from "react-helmet"
-import semikolan from '../json/semikolan.json'
+import Landing from "../component/Landing";
+import Join from "../component/Join";
+import MainLayout from "../layouts/MainLayout";
 
 export default function JoinPage() {
-    return (
-        <>
-          <Header />
-            <Landing
-              lottiename="join"
-              redirect="#work"
-              p1="Currently, We are Building"
-              p2="Our Own Ecosystem"
-              p3="Till then Join us through"
-              buttontext="Scroll Down"
-            />
-            <Join num="01." />
-            <Footer social={semikolan.social} />
-            <Helmet>
-              <title>Join | SemiKolan | Never Stop Building</title>
-            </Helmet>  
-        </>
-    )
+  return (
+    <>
+      <MainLayout title="Join Us | Never Stop Building">
+        <Landing
+          lottiename="join"
+          redirect="#work"
+          p1="Currently, We are Building"
+          p2="Our Own Ecosystem"
+          p3="Till then Join us through"
+          buttontext="Scroll Down"
+        />
+        <Join num="01." />
+      </MainLayout>
+    </>
+  );
 }
