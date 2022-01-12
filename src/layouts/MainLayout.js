22875bc3fb574127041aee0d2efc4a3c98d2fd75
Harsh Vishwakarma <64343449+meinhoonharsh@users.noreply.github.com>
2022-01-13 02:49:03 +0530

    Updated Main Layout

--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -1,13 +1,20 @@
 import React from 'react'
+import { Helmet } from 'react-helmet'
 import Footer from '../component/Footer'
 import Header from '../component/Header'
+import semikolan from "../json/semikolan.json";
 
 export default function MainLayout(props) {
     return (
         <>
             <Header />
             {props.children}
-            <Footer />
+            <Footer
+                social={props.social ?? semikolan.social}
+            />
+            <Helmet>
+                {props.title ?? 'SemiKolan | Never Stop Building'}
+            </Helmet>
 
         </>
     )
