886823f5393848bc95802bfc732d8c188c672c40
Harsh Vishwakarma <64343449+meinhoonharsh@users.noreply.github.com>
2022-01-13 03:10:35 +0530

    Implemented MainLayout on Initiatives

--- src/pages/Initiatives.js
+++ src/pages/Initiatives.js
@@ -1,30 +1,22 @@
-import React from "react";
-import Header from "../component/Header";
 import Landing from "../component/Landing";
-import Footer from "../component/Footer";
 import BongoCat from "../component/BongoCat";
-import { Helmet } from "react-helmet";
-import semikolan from "../json/semikolan.json";
 import Projects from "../component/Projects";
+import MainLayout from "../layouts/MainLayout";
 export default function Initiatives() {
   return (
     <>
-      <Header />
-      <Landing
-        lottiename="achievements"
-        redirect="#billi"
-        p1=""
-        p2="Our Initiatives"
-        p3="Things we're building for the world!"
-        buttontext="Have a Look"
-      />
-      <Projects />
-      <BongoCat />
-
-      <Footer social={semikolan.social} />
-      <Helmet>
-        <title>Initiatives | SemiKolan | Never Stop Building</title>
-      </Helmet>
+      <MainLayout title="Initiatives | SemiKolan">
+        <Landing
+          lottiename="achievements"
+          redirect="#billi"
+          p1=""
+          p2="Our Initiatives"
+          p3="Things we're building for the world!"
+          buttontext="Have a Look"
+        />
+        <Projects />
+        <BongoCat />
+      </MainLayout>
     </>
   );
 }
