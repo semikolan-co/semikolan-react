8653842a910eac5982a0a01faf0c2117e2c1bb32
Harsh Vishwakarma <64343449+meinhoonharsh@users.noreply.github.com>
2022-01-13 03:17:01 +0530

    Implemented MainLayout on Under Construction Page

--- src/pages/UnderConstruction.js
+++ src/pages/UnderConstruction.js
@@ -1,24 +1,15 @@
-import React from 'react'
-import { Helmet } from 'react-helmet'
-import Footer from '../component/Footer'
-import Header from '../component/Header'
 import BongoCat from "../component/BongoCat";
-import semikolan from "../json/semikolan.json";
-import Landing from '../component/Landing';
-
+import MainLayout from "../layouts/MainLayout";
 
 export default function UnderConstruction() {
-    return (
+  return (
     <>
-        <Header />
-        <BongoCat 
-            bongoTopMessage='Oops! This page is under construction🙁'
-            bongoBottomMessage='Our Bongo Cat is working hard to make it as awesome as possible.'
+      <MainLayout title="Under Construction | SemiKolan">
+        <BongoCat
+          bongoTopMessage="Oops! This page is under construction🙁"
+          bongoBottomMessage="Our Bongo Cat is working hard to make it as awesome as possible."
         />
-        <Footer social={semikolan.social} />
-        <Helmet>
-          <title>SemiKolan | Never Stop Building</title>
-        </Helmet>
-      </>
-    )
+      </MainLayout>
+    </>
+  );
 }
