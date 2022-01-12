a2da5144af8a791e0670db3bf84fdc14e37e6bc5
Harsh Vishwakarma <64343449+meinhoonharsh@users.noreply.github.com>
2022-01-13 03:13:43 +0530

    Implemented MainLayout on Contact Page

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,31 +1,23 @@
-import React from 'react'
-import Header from '../component/Header'
-import Landing from '../component/Landing'
-import Footer from '../component/Footer'
-import Map from '../component/Map'
-import ContactForm from '../component/ContactForm'
-import { Helmet } from "react-helmet"
-import semikolan from '../json/semikolan.json'
+import Landing from "../component/Landing";
+import Map from "../component/Map";
+import ContactForm from "../component/ContactForm";
+import MainLayout from "../layouts/MainLayout";
 
 export default function Contact() {
-    return (
-        <>
-            <Header />
-            <Landing
-              lottiename="contact"
-              redirect="#contact"
-              p1="Now You Would like to"
-              p2="Contact Us"
-              p3="Send your Query"
-              buttontext="Send Query"
-            />
-            <Map />
-            <ContactForm />
-
-            <Footer social={semikolan.social} />
-            <Helmet>
-              <title>Contact | SemiKolan | Never Stop Building</title>
-            </Helmet>
-        </>
-    )
+  return (
+    <>
+      <MainLayout title="Contact Us | SemiKolan">
+        <Landing
+          lottiename="contact"
+          redirect="#contact"
+          p1="Now You Would like to"
+          p2="Contact Us"
+          p3="Send your Query"
+          buttontext="Send Query"
+        />
+        <Map />
+        <ContactForm />
+      </MainLayout>
+    </>
+  );
 }
