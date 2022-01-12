3338c5908f99accee657ff50fc6f874208b864a1
Harsh Vishwakarma <64343449+meinhoonharsh@users.noreply.github.com>
2022-01-13 03:02:52 +0530

    Implemented MainLayout on Showcase Page

--- src/pages/Showcase.js
+++ src/pages/Showcase.js
@@ -1,29 +1,21 @@
-import React from 'react'
-import Header from '../component/Header'
-import Landing from '../component/Landing'
-import Footer from '../component/Footer'
-import Work from '../component/Work'
-import { Helmet } from "react-helmet"
-import semikolan from '../json/semikolan.json'
+import Landing from "../component/Landing";
+import Work from "../component/Work";
+import MainLayout from "../layouts/MainLayout";
 
 export default function Showcase() {
-    return (
-        <>
-           <Header />
-            <Landing
-              lottiename="work"
-              redirect="#work"
-              p1="Ding Ding! Presenting"
-              p2="Semikolan Showcase"
-              p3="Collection of our projects"
-              buttontext="View Projects"
-            />
-            <Work num="01." removeViewMore />
-
-            <Footer social={semikolan.social} />
-            <Helmet>
-              <title>Showcase | SemiKolan | Never Stop Building</title>
-            </Helmet>  
-        </>
-    )
+  return (
+    <>
+      <MainLayout title="SemiKolan | Never Stop Building">
+        <Landing
+          lottiename="work"
+          redirect="#work"
+          p1="Ding Ding! Presenting"
+          p2="Semikolan Showcase"
+          p3="Collection of our projects"
+          buttontext="View Projects"
+        />
+        <Work num="01." removeViewMore />
+      </MainLayout>
+    </>
+  );
 }
