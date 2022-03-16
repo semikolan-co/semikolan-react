11e7a55628f7b01316293f26216c054697db0802
Harsh Vishwakarma <64343449+meinhoonharsh@users.noreply.github.com>
2022-03-16 20:20:10 +0530

    Updated Communities and Added Dynamic Image import

--- src/component/Community.js
+++ src/component/Community.js
@@ -1,42 +1,53 @@
 import React from "react";
-import hackodhisa from '../assets/images/community/hackodhisa.jpg';
-import hackthisfall from '../assets/images/community/hackthisfall.png';
-import swoc from '../assets/images/community/swoc.png';
-import icc from '../assets/images/community/ICC.png';
-import gdscUitRGP from '../assets/images/community/gdsc.svg';
-import cncfBhopal from '../assets/images/community/cncf.svg';
 
 const COMMUNITY = [
   {
-    "title": "Hack Odisha",
-    "icon": hackodhisa,
-    "url": "https://hackodisha.xyz/"
+    "title": "Learn For Cause",
+    "icon": "logo_learnforcause.png",
+    "url": "https://www.learnforcause.com/"
   },
   {
-    "title": "Hack this fall",
-    "icon": hackthisfall,
-    "url": "https://hackthisfall.tech/"
-  },
-  {
-    "title": "Script winter of code",
-    "icon": swoc,
-    "url": "https://swoc.scriptindia.org/"
-  },
-  {
-    "title": "Infinite clone conspiracy",
-    "icon": icc,
-    "url": "https://iccta.co/"
+    "title": "Script Foundation",
+    "icon": "logo_scriptfoundation.png",
+    "url": "https://www.scriptindia.org/"
   },
   {
     "title": "CNCF Bhopal",
-    "icon": cncfBhopal,
-    "url": "https://community.cncf.io/bhopal/"
+    "icon": "logo_cncfbhopal.png",
+    "url": "https://community.cncf.io/bhopal"
+  },
+  {
+    "title": "ECell IET DAVV",
+    "icon": "logo_ecellietdavv.png",
+    "url": "https://ietdavv.edu.in/"
+  },
+  {
+    "title": "Eduhub",
+    "icon": "logo_eduhub.png",
+    "url": "https://eduhubcommunity.tech/"
+  },
+  {
+    "title": "Devscript",
+    "icon": "logo_devscript.png",
+    "url": "https://devscript.org/"
+  },
+  {
+    "title": "People who Codes",
+    "icon": "logo_peoplewhocodes.png",
+    "url": "https://peoplewho.codes/"
+  },
+  {
+    "title": "Flutter Bhopal",
+    "icon": "logo_flutterbhopal.png",
+    "url": "https://gdsc.community.dev/university-institute-of-technology-rgpv-bhopal/"
   },
   {
     "title": "GDSC UIT RGPV",
-    "icon": gdscUitRGP,
+    "icon": "logo_gdscuitrgpv.png",
     "url": "https://gdsc.community.dev/university-institute-of-technology-rgpv-bhopal/"
   }
+
+
 ];
 
 function Community(props) {
@@ -53,7 +64,7 @@ function Community(props) {
           return (
             <a href={community.url} target="_blank" className="communityUrl">
               <div className="communityICON" data-aos="fade-up" data-aos-duration={500}>
-                <img src={community.icon} alt={community.title} />
+                <img src={require('../assets/images/community/'+community.icon).default} alt={community.title} />
               </div>
             </a>
           );
