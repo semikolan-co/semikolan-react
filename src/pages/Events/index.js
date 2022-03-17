c5b63566e3ad6cbc6e25df9578be27c88a7c8f1e
Harsh Vishwakarma <64343449+meinhoonharsh@users.noreply.github.com>
2022-03-17 22:28:57 +0530

    Created Events Page Landing

--- src/pages/Events/index.js
+++ src/pages/Events/index.js
@@ -1,11 +1,17 @@
 import React from "react";
 import MainLayout from "../../layouts/MainLayout";
+import "./style.scss";
 
 export default function EventsPage() {
   return (
     <>
       <MainLayout title="SemiKolan | Never Stop Building">
-        <h2>Events Page</h2>
+        <div className="eventspage">
+          <div className="pagetitle">
+            <h1>Events</h1>
+            <span>Lorem ipsum dolor sit amet, consectetur adip</span>
+          </div>
+        </div>
       </MainLayout>
     </>
   );
