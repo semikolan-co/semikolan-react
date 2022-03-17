625a6be686549610b4a7fd5cc1e2f35efac211e1
Harsh Vishwakarma <64343449+meinhoonharsh@users.noreply.github.com>
2022-03-17 16:43:09 +0530

    Created Events Page

---
+++ src/pages/EventsPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MainLayout from "../../layouts/MainLayout";
+
+export default function EventsPage() {
+  return (
+    <>
+      <MainLayout title="SemiKolan | Never Stop Building">
+        <h2>Events Page</h2>
+      </MainLayout>
+    </>
+  );
+}
