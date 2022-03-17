028970f809061c11290a76537d83bce096bc56fa
Harsh Vishwakarma <64343449+meinhoonharsh@users.noreply.github.com>
2022-03-17 23:46:46 +0530

    Dyanamically rendered events ion Events Page

--- src/pages/Events/index.js
+++ src/pages/Events/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import MainLayout from "../../layouts/MainLayout";
 import "./style.scss";
+import events from '../../json/events.json';
 
 export default function EventsPage() {
   return (
@@ -10,6 +11,30 @@ export default function EventsPage() {
           <div className="pagetitle">
             <h1>Events</h1>
             <span>Lorem ipsum dolor sit amet, consectetur adip</span>
+          </div>
+
+          <div className="container">
+            {
+              events.map((event, index) => {
+                return (
+                  <div className="eventbody" key={index}>
+                    <div className="eventimage">
+                      <img src={require("../../assets/images/events/"+event.cover_image).default} alt={event.title} />
+                    </div>
+                    <div className="eventcontent">
+                      <h2>{event.title}</h2>
+                      <p>{event.desc}</p>
+                      <div>
+
+                      <a href={event.registration_link} className="button"> {event.registration_text}</a>
+                      <a href={event.event_link} className="button"> Join </a>
+                      </div>
+                    </div>
+                  </div>
+                )
+              })
+            }
+
           </div>
         </div>
       </MainLayout>
