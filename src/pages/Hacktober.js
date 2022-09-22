0be6b3c78e03626e6e2bbf099032ea6a807e282a
Prakrti Mankar <fancymess29@gmail.com>
2022-09-23 02:41:27 +0530

    hacktober projrct section

--- src/pages/Hacktober.js
+++ src/pages/Hacktober.js
@@ -3,11 +3,23 @@ import "../assets/css/Hacktober.css";
 
 const Hacktober = () => {
 
+    // project card data
+    const project_card = [
+        {
+            id: 1,
+            icon: '',
+            title: 'Express delivery innovative Design service',
+            detail: 'Express delivery inno service effective logistics solution for delivery of small cargo delivery service.',
+        },
+    ]
+
     return (
         <div>
             <p>
                 Hacktober
             </p>
+
+
 
             {/* about section */}
             <section class="about-section">
@@ -17,11 +29,18 @@ const Hacktober = () => {
                             <div class="inner-column">
                                 <div class="sec-title">
                                     <span class="title">About Css3transition</span>
-                                    <h2 style={{color: '#ffffff'}}>We are Creative Tech Enthusiast working since 2015</h2>
+                                    <h2 style={{ color: '#ffffff' }}>What is Hacktoberfest?</h2>
                                 </div>
-                                <div class="text">I am Rahul Yaduvanshi works at Css3 Transition since last 3 years. We are here to provide touch notch solution for your website or web application that helps you to make your website look attractive & efficient in handling by creating usefull plugins thats you need.</div>
                                 <div class="text">
-                                    We are here to serve you next level tutorial that currently in trend to match you with your expertise. Css3 transition is a learning website. where you can find many good quality content related to web development and tutorials about plugins. here we are using html, html5, css, css3, jquery & javascript along with inspirational UI design layout by professionals by using Photoshop and adobe allustrator.
+                                    Hacktoberfest under Digital Ocean is back again with their one of the most awaited and splendid event for this forthcoming autumn!
+                                    ‘Hacktoberfest’, a synonym for ‘Open Source’, ‘Contributions’, ‘Projects’ and much more, all on your fingertips.
+
+                                    It’s a wide umbrella for all those techy geeks grinding extensively round the globe, to connect and work on some amazing open source projects and generate some remarkable real world solutions.
+                                    Join this ‘Extraordinary Fest’ for not so ‘ordinary’ geeks, who challenge their limits and donate their time to support open source projects for this 1 month long event.
+                                    Taking under regards to set in motion this chapter of Hacktoberfest, we the Google Developer Student Club partnered with SemiKolan has taken the initiative to cultivate a sense of spark in the young minds regarding Open Source and the importance of being a part of Community.
+                                    Henceforth we’ve planned a month long journey which is embedded with multiple events and sessions which is going to revolve around stuff related to new technologies, Open Source and much more!
+
+
                                 </div>
                                 <div class="btn-box">
                                     <a href="#" class="theme-btn btn-style-one">Contact Us</a>
@@ -44,28 +63,110 @@ const Hacktober = () => {
                         </div>
 
                     </div>
-                    <div class="sec-title">
-                        <span class="title">Our Future Goal</span>
-                        <h2>We want to lead in innovation & Technology</h2>
-                    </div>
-                    <div class="text">
-                        We works on UI/UX and functionality as well so that a plugins comes with proper stucture & stunning looks which suits to your web app & website.
-                    </div>
-                    <div class="text">
-                        We take a small toolkit here and ride it well so that it is fit for your use. One who performs well and looks even better.
-                    </div>
-                    <div class="text">
-                        Here we are trying to give you all kinds of technical content, whether it is related to designing or functionality. We are creating content on a lot of languages and will continue to make it free of cost even if you use it without any problem. Which is a very important thing.
-                    </div>
-                    <div class="text">
-                        Here you can also share the content you create, if our technical team likes it, then we will also share it on our blog.
-                    </div>
-                    <div class="text">
+                </div>
+            </section>
 
-                        In the end, I would say keep visiting our website and enjoy the quality content.
+            {/* project section */}
+
+            <section class="section-services" style={{ backgroundColor: '#0B1B33' }}>
+                <div class="container">
+                    <div class="row justify-content-center text-center">
+                        <div class="col-md-10 col-lg-8">
+                            <div class="header-section">
+                                <h2 class="title">Projects <span>Repository</span></h2>
+                                <p class="description">There are many variations of passages of Lorem Ipsum available but the majority have suffered alteration in some injected humour</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        {/* <!-- Start Single Service --> */}
+                        <div class="col-md-6 col-lg-4">
+                            <div class="single-service" style={{background: "#233554"}}>
+                                <div class="part-1">
+                                    <i class="fab fa-500px"></i>
+                                    <h3 class="title">Express delivery innovative Design service</h3>
+                                </div>
+                                <div class="part-2">
+                                    <p class="description">Express delivery inno service effective logistics solution for delivery of small cargo delivery service.</p>
+                                    <a href="#"><i class="fas fa-arrow-circle-right"></i>Read More</a>
+                                </div>
+                            </div>
+                        </div>
+                        {/* <!-- / End Single Service --> */}
+                        {/* <!-- Start Single Service --> */}
+                        <div class="col-md-6 col-lg-4">
+                            <div class="single-service">
+                                <div class="part-1">
+                                    <i class="fab fa-angellist"></i>
+                                    <h3 class="title">Online chat may refer to any kind communication</h3>
+                                </div>
+                                <div class="part-2">
+                                    <p class="description">Express delivery inno service effective logistics solution for delivery of small cargo delivery service.</p>
+                                    <a href="#"><i class="fas fa-arrow-circle-right"></i>Read More</a>
+                                </div>
+                            </div>
+                        </div>
+                        {/* <!-- / End Single Service --> */}
+                        {/* <!-- Start Single Service --> */}
+                        <div class="col-md-6 col-lg-4">
+                            <div class="single-service">
+                                <div class="part-1">
+                                    <i class="fas fa-award"></i>
+                                    <h3 class="title">Service provider provide organizations consulting</h3>
+                                </div>
+                                <div class="part-2">
+                                    <p class="description">Express delivery inno service effective logistics solution for delivery of small cargo delivery service.</p>
+                                    <a href="#"><i class="fas fa-arrow-circle-right"></i>Read More</a>
+                                </div>
+                            </div>
+                        </div>
+                        {/* <!-- / End Single Service --> */}
+                        {/* <!-- Start Single Service --> */}
+                        <div class="col-md-6 col-lg-4">
+                            <div class="single-service">
+                                <div class="part-1">
+                                    <i class="fab fa-asymmetrik"></i>
+                                    <h3 class="title">Express delivery innovative Design service</h3>
+                                </div>
+                                <div class="part-2">
+                                    <p class="description">Express delivery inno service effective logistics solution for delivery of small cargo delivery service.</p>
+                                    <a href="#"><i class="fas fa-arrow-circle-right"></i>Read More</a>
+                                </div>
+                            </div>
+                        </div>
+                        {/* <!-- / End Single Service --> */}
+                        {/* <!-- Start Single Service --> */}
+                        <div class="col-md-6 col-lg-4">
+                            <div class="single-service">
+                                <div class="part-1">
+                                    <i class="fas fa-broadcast-tower"></i>
+                                    <h3 class="title">Provide solutions and services to end users</h3>
+                                </div>
+                                <div class="part-2">
+                                    <p class="description">Express delivery inno service effective logistics solution for delivery of small cargo delivery service.</p>
+                                    <a href="#"><i class="fas fa-arrow-circle-right"></i>Read More</a>
+                                </div>
+                            </div>
+                        </div>
+                        {/* <!-- / End Single Service --> */}
+                        {/* <!-- Start Single Service --> */}
+                        <div class="col-md-6 col-lg-4">
+                            <div class="single-service">
+                                <div class="part-1">
+                                    <i class="fab fa-canadian-maple-leaf"></i>
+                                    <h3 class="title">This broad term incorporates all IT businesses</h3>
+                                </div>
+                                <div class="part-2">
+                                    <p class="description">Express delivery inno service effective logistics solution for delivery of small cargo delivery service.</p>
+                                    <a href="#"><i class="fas fa-arrow-circle-right"></i>Read More</a>
+                                </div>
+                            </div>
+                        </div>
+                        {/* <!-- / End Single Service --> */}
                     </div>
                 </div>
             </section>
+
         </div>
     )
 }
