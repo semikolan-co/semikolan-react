e608c778df181da30619aa28f0cb344ef209fdb8
Satyam Sharma <satyamsharma1725@gmail.com>
2022-10-10 21:34:06 +0530

    file name changed

--- src/pages/Hacktober.js
+++ src/pages/Hacktober.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import "../assets/css/Hacktober.css";
+import "../assets/css/hacktober.css";
 import register from "../assets/images/events/register.png";
 import Hacktober_Heading from "../assets/images/events/HacktoberHeading.png";
 import Hacktober_Year from "../assets/images/events/HacktoberYear.png";
