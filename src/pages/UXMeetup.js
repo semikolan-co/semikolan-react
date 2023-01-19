9a69f14806ed4c58feabe8ac3a6200adddcdc956
Satyam Sharma <satyamsharma1725@gmail.com>
2023-01-19 15:29:15 +0530

    speakers added

--- src/pages/UXMeetup.js
+++ src/pages/UXMeetup.js
@@ -5,6 +5,7 @@ import UXLanding from "../component/UXLanding";
 import UXAbout from "../component/UXAbout";
 import UXLayout from "../layouts/UXLayout";
 import UXCommunity from "../component/UXCommunity.js";
+import UXSpeakers from "../component/UXSpeakers";
 import uxmeetup from "../json/uxmeetup.json";
 import Venue from "../component/Venue";
 
@@ -26,6 +27,11 @@ export default function UXMeetup() {
           titleline="About"
           content={uxmeetup.about.content}
           removeViewMore
+        />
+
+        <UXSpeakers
+          num="02."
+          speakers={uxmeetup.speakers.slice(0, 6)}
         />
 
         <UXCommunity num="03." Community={uxmeetup.community.slice(0, 4)} />
